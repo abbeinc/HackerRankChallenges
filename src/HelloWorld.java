bc4466ef9aeedd955d9a7540de76873f2a483b8c
abbeinc <abbeinc@gmail.com>
2022-12-09 03:11:12 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] ars){
+
+
+
+        Scanner scn = new Scanner(System.in);
+        String sentence = scn.nextLine();
+        scn.close();
+        System.out.println("Hello World");
+        System.out.println(sentence);
+
+
+
+
+
+
+    }
+
+
+
+}
