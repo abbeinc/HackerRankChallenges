639aea90102c7c849f5d2586feabcf3e294e7a9a
abbeinc <abbeinc@gmail.com>
2022-12-09 06:03:15 -0500

    2nd task

---
+++ src/ScannerTask.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ScannerTask {
+
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            System.out.println(a);
+            a = scan.nextInt();
+            System.out.println(a);
+            a = scan.nextInt();
+            System.out.println(a);
+            scan.close();
+
+
+
+
+        }
+    }
+
+
+
